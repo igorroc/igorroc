511c04425955058166f65070dd913465f71ff24f
IgorRoc <igor_roc@hotmail.com.br>
2021-05-04 12:08:03 -0300

    [add]: Topic translation

--- translation.js
+++ translation.js
@@ -9,16 +9,38 @@ function translation() {
         .then(Response => Response.json())
         .then(data => {
             let i = 0
+            let j = 0
             isPortuguese = isPortuguese ? false : true // TOGGLE THE LANGUAGE
             contact.innerHTML = data.contact[`${isPortuguese ? "br" : "us"}`] // CONTACT INFO
+            footer.children[0].innerHTML = data.footer[`${isPortuguese ? "br" : "us"}`] // FOOTER
+
             for (const topic of topics) { // EACH TOPIC
                 if(data.topics[i].title != undefined){
                     topic.children[0].children[1].innerHTML = data.topics[i].title[`${isPortuguese ? "br" : "us"}`]
-                    console.log(topic.children[1])
+                    if(topic.children[1].children[0].localName == 'ul'){
+                        j = 0
+                        for (const list of topic.children[1].children[0].children) {
+                            console.log(list.className)
+                            if (list.className == "divisao") {
+                                if (list.children[0].children.length > 0) {
+                                    list.children[0].children[1].innerHTML = data.topics[i].itens[j].key[`${isPortuguese ? "br" : "us"}`]
+                                    list.children[1].innerHTML = data.topics[i].itens[j].value[`${isPortuguese ? "br" : "us"}`]
+                                }else{
+                                    console.log(list.children[0])
+                                    list.children[0].innerHTML = data.topics[i].itens[j].key[`${isPortuguese ? "br" : "us"}`]
+                                }
+                                
+                            }
+                            j++
+                        }
+                        console.log('------')
+                    }else if (topic.children[1].children[0].localName == 'destaque') {
+                        topic.children[1].innerHTML = data.topics[i].itens[0].key[`${isPortuguese ? "br" : "us"}`]
+                    }
                 }
                 i++
             }
-            footer.children[0].innerHTML = data.footer[`${isPortuguese ? "br" : "us"}`]
+            
             
         })
     
