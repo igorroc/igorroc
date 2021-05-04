850669ee6f04be7f671fa7e154d17d0562338675
IgorRoc <igor_roc@hotmail.com.br>
2021-05-04 09:13:29 -0300

    [add]: Translation of "contact", "title", "footer"

--- translation.js
+++ translation.js
@@ -1,31 +1,25 @@
 var isPortuguese = true
 var contact = document.getElementById("title-contact")
 var topics = document.getElementsByClassName("topico")
-var rodape = document.getElementsByClassName('rodape')[0]
+var footer = document.getElementsByClassName('rodape')[0]
 
 
 function translation() {
-    if(isPortuguese){
-        isPortuguese = false
-        fetch("./translation.json")
+    fetch("./translation.json")
         .then(Response => Response.json())
         .then(data => {
             let i = 0
-            contact.innerHTML = data.contact.us // CONTACT INFO
+            isPortuguese = isPortuguese ? false : true // TOGGLE THE LANGUAGE
+            contact.innerHTML = data.contact[`${isPortuguese ? "br" : "us"}`] // CONTACT INFO
             for (const topic of topics) { // EACH TOPIC
                 if(data.topics[i].title != undefined){
-                    console.log(topic)
-                    topic.children[0].children[1].innerHTML = data.topics[i].title.us
+                    topic.children[0].children[1].innerHTML = data.topics[i].title[`${isPortuguese ? "br" : "us"}`]
+                    console.log(topic.children[1])
                 }
                 i++
             }
+            footer.children[0].innerHTML = data.footer[`${isPortuguese ? "br" : "us"}`]
+            
         })
-    }else{
-        isPortuguese = true
-        fetch("./translation.json")
-        .then(Response => Response.json())
-        .then(data => {
-            contact.innerHTML = data.contact.br
-        })
-    }
+    
 }
