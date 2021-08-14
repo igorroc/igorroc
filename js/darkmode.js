78781d177f546cedb32a044b6a89c77c3db811c0
IgorRoc <igor_roc@hotmail.com.br>
2021-08-14 13:38:14 -0300

    🎨[style]: Prettier

--- js/darkmode.js
+++ js/darkmode.js
@@ -4,29 +4,28 @@ var body = document.body
 var links = document.links
 var iconLinks = document.getElementsByClassName("link")
 var topicos = document.getElementsByClassName("descricao")
-var logo = document.getElementById('logo')
-var rodape = document.getElementsByClassName('rodape')[0]
-
+var logo = document.getElementById("logo")
+var rodape = document.getElementsByClassName("rodape")[0]
 
 function darkMode() {
-    isDark = isDark ? false : true
+	isDark = isDark ? false : true
 
-    body.classList.toggle("dark-mode-bg")
-    body.classList.toggle("dark-mode-txt")
-    logo.classList.toggle("svg-white")
+	body.classList.toggle("dark-mode-bg")
+	body.classList.toggle("dark-mode-txt")
+	logo.classList.toggle("svg-white")
 
-    for (const link of links) {
-        link.classList.toggle("dark-mode-txt")
-    }
-    for (const topico of topicos) {
-        var icons = topico.getElementsByTagName('img')
-        var bars = topico.getElementsByClassName('barra')
+	for (const link of links) {
+		link.classList.toggle("dark-mode-txt")
+	}
+	for (const topico of topicos) {
+		var icons = topico.getElementsByTagName("img")
+		var bars = topico.getElementsByClassName("barra")
 
-        for (const icon of icons) {
-            icon.classList.toggle("svg-white")
-        }
-        for (const bar of bars) {
-            bar.classList.toggle("dark-bar")
-        }
-    }
+		for (const icon of icons) {
+			icon.classList.toggle("svg-white")
+		}
+		for (const bar of bars) {
+			bar.classList.toggle("dark-bar")
+		}
+	}
 }
--- js/translation.js
+++ js/translation.js
@@ -1,51 +1,72 @@
 var isPortuguese = true
 var contact = document.getElementById("title-contact")
 var topics = document.getElementsByClassName("topico")
-var footer = document.getElementsByClassName('rodape')[0]
-
+var footer = document.getElementsByClassName("rodape")[0]
 
 function translation() {
-    fetch("../translation.json")
-        .then(Response => Response.json())
-        .then(data => {
-            let i = 0
-            let j = 0
-            isPortuguese = isPortuguese ? false : true // TOGGLE THE LANGUAGE
-            contact.innerHTML = data.contact[`${isPortuguese ? "br" : "us"}`] // CONTACT INFO
-            footer.children[0].innerHTML = data.footer[`${isPortuguese ? "br" : "us"}`] // FOOTER
+	fetch("../translation.json")
+		.then((Response) => Response.json())
+		.then((data) => {
+			let i = 0
+			let j = 0
+			isPortuguese = isPortuguese ? false : true // TOGGLE THE LANGUAGE
+			contact.innerHTML = data.contact[`${isPortuguese ? "br" : "us"}`] // CONTACT INFO
+			footer.children[0].innerHTML =
+				data.footer[`${isPortuguese ? "br" : "us"}`] // FOOTER
 
-            for (const topic of topics) { // EACH TOPIC
-                if(data.topics[i].title != undefined){
-                    topic.children[0].children[1].innerHTML = data.topics[i].title[`${isPortuguese ? "br" : "us"}`]
-                    if(topic.children[1].children[0].localName == 'ul'){
-                        j = 0
-                        for (const list of topic.children[1].children[0].children) {
-                            if (list.className == "divisao") {
-                                if (list.children[0].children.length > 0) {
-                                    if(list.children[0].children[1]){
-                                        list.children[0].children[1].innerHTML = data.topics[i].itens[j].key[`${isPortuguese ? "br" : "us"}`]
-                                    }else{
-                                        list.children[0].children[0].innerHTML = data.topics[i].itens[j].key[`${isPortuguese ? "br" : "us"}`]
-                                    }
-                                    if(data.topics[i].itens[j].value){
-                                        list.children[1].innerHTML = data.topics[i].itens[j].value[`${isPortuguese ? "br" : "us"}`]
-                                    }
-                                }else{
-                                    list.children[0].innerHTML = data.topics[i].itens[j].key[`${isPortuguese ? "br" : "us"}`]
-                                    if(data.topics[i].itens[j].value){
-                                        list.children[1].innerHTML = data.topics[i].itens[j].value[`${isPortuguese ? "br" : "us"}`]
-                                    }
-                                }
-                            }
-                            j++
-                        }
-                    }else if (topic.children[1].children[0].localName == 'destaque') {
-                        topic.children[1].innerHTML = data.topics[i].itens[0].key[`${isPortuguese ? "br" : "us"}`]
-                    }
-                }
-                i++
-            }
-            
-            
-        })
+			for (const topic of topics) {
+				// EACH TOPIC
+				if (data.topics[i].title != undefined) {
+					topic.children[0].children[1].innerHTML =
+						data.topics[i].title[`${isPortuguese ? "br" : "us"}`]
+					if (topic.children[1].children[0].localName == "ul") {
+						j = 0
+						for (const list of topic.children[1].children[0]
+							.children) {
+							if (list.className == "divisao") {
+								if (list.children[0].children.length > 0) {
+									if (list.children[0].children[1]) {
+										list.children[0].children[1].innerHTML =
+											data.topics[i].itens[j].key[
+												`${isPortuguese ? "br" : "us"}`
+											]
+									} else {
+										list.children[0].children[0].innerHTML =
+											data.topics[i].itens[j].key[
+												`${isPortuguese ? "br" : "us"}`
+											]
+									}
+									if (data.topics[i].itens[j].value) {
+										list.children[1].innerHTML =
+											data.topics[i].itens[j].value[
+												`${isPortuguese ? "br" : "us"}`
+											]
+									}
+								} else {
+									list.children[0].innerHTML =
+										data.topics[i].itens[j].key[
+											`${isPortuguese ? "br" : "us"}`
+										]
+									if (data.topics[i].itens[j].value) {
+										list.children[1].innerHTML =
+											data.topics[i].itens[j].value[
+												`${isPortuguese ? "br" : "us"}`
+											]
+									}
+								}
+							}
+							j++
+						}
+					} else if (
+						topic.children[1].children[0].localName == "destaque"
+					) {
+						topic.children[1].innerHTML =
+							data.topics[i].itens[0].key[
+								`${isPortuguese ? "br" : "us"}`
+							]
+					}
+				}
+				i++
+			}
+		})
 }
