f9f3ca4d92a438cd5e6c613cfe96bfe6dc22cfda
IgorRoc <igor_roc@hotmail.com.br>
2021-08-16 10:42:29 -0300

    [add]: Mobile swipe

--- js/index.js
+++ js/index.js
@@ -13,9 +13,9 @@ let currentPage = page || 0
 let transitioning = false
 let isMobile = isMobileDevice()
 
-
 window.scrollTo(0, 0)
 nextPage()
+
 document.addEventListener("wheel", (event) => {
 	if (!transitioning) {
 		transitioning = true
@@ -41,17 +41,18 @@ document.addEventListener("wheel", (event) => {
 function nextPage() {
 	window.history.pushState("", `page ${currentPage}`, `?page=${currentPage}`)
 
-	if(!isMobile){
+	if (!isMobile) {
 		wrapperPages.setAttribute(
 			"style",
 			`transform: translateY(${-100 * currentPage}vh)`
 		)
-	}else{
+	} else {
 		wrapperPages.setAttribute(
 			"style",
 			`transform: translateX(${-100 * currentPage}vw)`
 		)
 	}
+
 	timeline.querySelector(".timelineActive").classList.remove("timelineActive")
 
 	timeline.children[currentPage].classList.add("timelineActive")
@@ -74,7 +75,7 @@ document.addEventListener("mousemove", (event) => {
 	})
 })
 
-window.addEventListener("resize", ()=> {
+window.addEventListener("resize", () => {
 	isMobile = isMobileDevice()
 	nextPage()
 })
@@ -88,12 +89,74 @@ formClose.addEventListener("click", () => {
 	form.classList.remove("formShow")
 })
 
-
 function isMobileDevice() {
-    var check = false;
-    if(window.innerWidth <= 1200){
-		check = true;
+	var check = false
+	if (window.innerWidth <= 1200) {
+		check = true
 	}
-	console.log(check)
-    return check;
-};
+	return check
+}
+
+// mobile swipe
+document.addEventListener("touchstart", handleTouchStart, false)
+document.addEventListener("touchmove", handleTouchMove, false)
+
+var xDown = null
+var yDown = null
+
+function getTouches(evt) {
+	return (
+		evt.touches || // browser API
+		evt.originalEvent.touches
+	) // jQuery
+}
+
+function handleTouchStart(evt) {
+	const firstTouch = getTouches(evt)[0]
+	xDown = firstTouch.clientX
+	yDown = firstTouch.clientY
+}
+
+function handleTouchMove(evt) {
+	if (!xDown || !yDown) {
+		return
+	}
+
+	var xUp = evt.touches[0].clientX
+	var yUp = evt.touches[0].clientY
+
+	var xDiff = xDown - xUp
+	var yDiff = yDown - yUp
+
+	if (Math.abs(xDiff) > Math.abs(yDiff)) {
+		/*most significant*/
+		if (!transitioning) {
+			transitioning = true
+
+			if (xDiff > 0) {
+				/* right swipe */
+				currentPage++
+			} else {
+				/* left swipe */
+				currentPage--
+			}
+
+			currentPage >= timeline.children.length
+				? (currentPage = 0)
+				: (currentPage = currentPage)
+
+			currentPage < 0
+				? (currentPage = timeline.children.length - 1)
+				: (currentPage = currentPage)
+
+			nextPage()
+		}
+
+		setTimeout(() => {
+			transitioning = false
+		}, 1000)
+	}
+	/* reset values */
+	xDown = null
+	yDown = null
+}
