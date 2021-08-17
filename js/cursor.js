993882d7caf5685176b4f0744f7b2b4305e537e3
IgorRoc <igor_roc@hotmail.com.br>
2021-08-17 10:20:38 -0300

    [add]: Hidden texts

--- js/cursor.js
+++ js/cursor.js
@@ -1,17 +1,17 @@
 let root = document.documentElement
 
 let cursor = document.querySelector("#cursor")
-let links = document.querySelectorAll("a")
-let buttons = document.querySelectorAll("button")
-let textInputs = document.querySelectorAll("input")
-let textAreas = document.querySelectorAll("textarea")
-let cards = document.querySelectorAll(".card")
+
+const cursorSize = cursor.offsetWidth
+
+let links = document.querySelectorAll(".isClickable")
+let textInputs = document.querySelectorAll(".isTextInput")
 
 let mouseHold = false
 
 if (isMobile) {
 	cursor.classList.add("cursorMobile")
-}else{
+} else {
 	root.style.setProperty("cursor", "none")
 }
 
@@ -21,35 +21,15 @@ window.addEventListener("mousemove", (event) => {
 })
 
 links.forEach((link) => {
+	if (link.classList.contains("card")) {
+		link = link.querySelector("div > img")
+	}
 	link.addEventListener("mouseenter", () => {
 		cursor.classList.add("cursorOnLink")
 	})
 	link.addEventListener("mouseout", () => {
 		cursor.classList.remove("cursorOnLink")
 	})
-})
-
-buttons.forEach((button) => {
-	button.addEventListener("mouseenter", () => {
-		cursor.classList.add("cursorOnLink")
-	})
-	button.addEventListener("mouseout", () => {
-		cursor.classList.remove("cursorOnLink")
-	})
-})
-
-cards.forEach((card) => {
-	if(card.classList.contains("isClickable")){
-		card = card.querySelector("div > img")
-		card.addEventListener("mouseenter", () => {
-			console.log("enter")
-			cursor.classList.add("cursorOnLink")
-		})
-		card.addEventListener("mouseout", () => {
-			console.log("leave")
-			cursor.classList.remove("cursorOnLink")
-		})
-	}
 })
 
 textInputs.forEach((input) => {
@@ -61,22 +41,34 @@ textInputs.forEach((input) => {
 	})
 })
 
-textAreas.forEach((textArea) => {
-	textArea.addEventListener("mouseenter", () => {
-		cursor.classList.add("cursorOnInput")
-	})
-	textArea.addEventListener("mouseout", () => {
-		cursor.classList.remove("cursorOnInput")
-	})
-})
-
 window.addEventListener("mousedown", () => {
 	cursor.classList.add("cursorClick")
 })
 
 window.addEventListener("mouseup", () => {
 	setTimeout(() => {
-
 		cursor.classList.remove("cursorClick")
 	}, 200)
+})
+
+let wrapperHiddenTexts = document.querySelectorAll(".wrapperHiddenText")
+let isHoveringHiddenText = false
+
+wrapperHiddenTexts.forEach((wrapperHiddenText) => {
+	wrapperHiddenText.addEventListener("mousemove", (e) => {
+		isHoveringHiddenText = true
+		wrapperHiddenText.style.clipPath = `inset(${
+			e.offsetY - cursorSize / 2
+		}px ${wrapperHiddenText.offsetWidth - e.offsetX - cursorSize / 2}px ${
+			wrapperHiddenText.offsetHeight - e.offsetY - cursorSize / 2
+		}px ${e.offsetX - cursorSize / 2}px)`
+	})
+	wrapperHiddenText.addEventListener("mouseout", () => {
+		setTimeout(() => {
+			if (!isHoveringHiddenText) {
+				wrapperHiddenText.style.clipPath = "inset(0)"
+			}
+		}, 1000)
+		isHoveringHiddenText = false
+	})
 })
