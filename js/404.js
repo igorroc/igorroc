caf7889dd8cf58f273a72848d6eee83d1d459894
IgorRoc <igor_roc@hotmail.com.br>
2021-08-17 08:09:57 -0300

    [fix]: 404 page with components files

--- js/404.js
+++ js/404.js
@@ -2,6 +2,6 @@ let h2 = document.querySelector(".pageActive h2")
 
 let url = document.location
 
-let cleanPath = url.pathname.split("/igorroc")[1]
+let cleanPath = url.pathname.split("/igorroc")[1] || url.pathname
 
 h2.innerHTML = `Page <code>${cleanPath}</code> not found`
