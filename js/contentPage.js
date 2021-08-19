62aa20574cafd00dfff74170a5876655f203a89a
IgorRoc <igor_roc@hotmail.com.br>
2021-08-19 09:04:29 -0300

    [add]: Hide "scrollMore" button when scroll

--- js/contentPage.js
+++ js/contentPage.js
@@ -5,5 +5,26 @@ setTimeout(() => {
 }, 1000)
 
 setTimeout(() => {
-    contentCtaButton?.classList.add("showButton")
+	contentCtaButton?.classList.add("showButton")
 }, 750)
+
+let timelineButtons = document.querySelectorAll("#timelineButtons > button")
+
+timelineButtons.forEach((button) => {
+	button.addEventListener("click", () => {
+		timelineButtons.forEach((b) => {
+			b.classList.remove("buttonContentActive")
+		})
+		button.classList.add("buttonContentActive")
+	})
+})
+
+window.addEventListener("scroll", (ev) => {
+	if (window.scrollY > 0) {
+		console.log("remover")
+        scrollMore.classList.add("scrollMoreHidden")
+	} else {
+        console.log("colocar")
+        scrollMore.classList.remove("scrollMoreHidden")
+	}
+}, false)
